b08df023240d15fbc6435041ed8db4fc89e8ad4e
Quan <wangquan@meifute.com>
2026-07-07 17:58:15 +0800

    feat: add prompt template manager view

--- Sources/CodexPlusApp/Workbench/WorkbenchMetrics.swift
+++ Sources/CodexPlusApp/Workbench/WorkbenchMetrics.swift
@@ -6,6 +6,8 @@ enum WorkbenchMetrics {
     static let conversationCornerRadius = CGFloat(24)
     static let composerCornerRadius = CGFloat(22)
     static let projectCardCornerRadius = CGFloat(18)
+    static let settingsCornerRadius = CGFloat(18)
+    static let settingsSidebarWidth = CGFloat(300)
     static let errorCornerRadius = CGFloat(8)
     static let composerControlHeight = CGFloat(30)
 }
